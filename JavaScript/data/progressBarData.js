94320db390ef480d6b0c782111317b9693ab5e9e
Dovilė <dovile.keparute@gmail.com>
2021-08-17 17:43:42 +0300

    Progress bars Data

---
+++ JavaScript/data/progressBarData.js
@@ -0,0 +1,19 @@
+const progressBarData = [
+    {
+        label: "UX design",
+        value: 90,
+    },
+    {
+        label: "Web Design",
+        value: 85,
+    },
+    {
+        label:"Web Development",
+        value: 86,
+    },
+    {
+        label: "Web Errors",
+        value: 5,
+    },
+];
+export { progressBarData }
